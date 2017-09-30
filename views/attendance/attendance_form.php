dd0af4aa67d2052700f5a70392641cf3356aa3a8
Scott123180 <Scott123180@gmail.com>
2017-09-30 14:34:42 -0400

    migrating previous code to bootstrap 4

--- views/attendance/attendance_form.php
+++ views/attendance/attendance_form.php
@@ -6,39 +6,47 @@ include('header.php');
 
 ?>
 
-    <div class="container">
-        <div class="panel panel-default col-12">
-            <!-- Default panel contents -->
-            <div class="panel-heading h2 text-center">Stop the Beatdown: Class Roster</div>
+    <div class="container col-12">
+            <!-- Default container contents -->
+            <div class="h2 text-center">Stop the Beatdown: Class Roster</div>
 
-            <!-- Table -->
-            <table class="table row table-hover table-bordered table-striped">
-                <colgroup>
-                    <col class="col-2">
-                    <col class="col-8">
-                    <col class="col-2">
-                </colgroup>
-                <tbody>
-                <tr>
-                    <th>Present</th>
-                    <th>Name</th>
-                    <th></th>
-                </tr>
-                <?php
-                     //TODO: pull info from DB
-                ?>
-                <tr>
-                    <td>
-                        <input type="checkbox" class="form-check-inline" aria-label="present">
-                    </td>
-                    <td>Jimmy Neutron</td>
-                    <td>
-                        <a href="#">More Details...</a>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
+        <div class="card">
+            <div class="card-block">
+                <!-- Table -->
+                <div class="table">
+                    <table class="table-responsive table-hover table-striped">
+                        <colgroup>
+                            <col class="col-2">
+                            <col class="col-8">
+                            <col class="col-2">
+                        </colgroup>
+                        <thead>
+                            <tr>
+                                <th>Present</th>
+                                <th>Name</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        //TODO: pull info from DB
+                        ?>
+                        <tr>
+                            <td>
+                                <input type="checkbox" class="form-check-inline" aria-label="present">
+                            </td>
+                            <td>Jimmy Neutron</td>
+                            <td>
+                                <a href="#">More Details...</a>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <!-- /Table -->
+                </div>
+            </div>
         </div>
+
 
         <div class="row col-12">
             <div class="col-9"></div>
