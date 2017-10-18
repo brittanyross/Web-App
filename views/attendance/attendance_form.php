d86269947712aed66980a209ef870ec1b4af21cf
Scott123180 <Scott123180@gmail.com>
2017-10-18 18:13:27 -0400

    off to class

--- views/attendance/attendance_form.php
+++ views/attendance/attendance_form.php
@@ -97,6 +97,19 @@ $selected_class = $_POST['classes'];
             </div>
         </div>
         </form>
+
+        <div class="row">
+            <div class="jumbotron" style="max-width: 700px; width: 100%; margin: 10px auto" >
+                <h4 class="secondary-title">Search for Participants</h4>
+                <br />
+                <form class="search-agency" method="POST" action="/agency-requests">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="searchquery" placeholder="Begin typing participant's name...">
+                    </div>
+                    <button type="submit" class="btn cpca form-control">Submit</button>
+                </form>
+            </div>
+        </div>
     </div>
 
 
