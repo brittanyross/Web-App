98686a144ab149c81ce2dd7a1bb34ef24085e6d4
vmjoseph <valliejoseph@gmail.com>
2017-11-02 15:46:20 -0400

    updating inactive function

--- views/agency-requests/view_participant.php
+++ views/agency-requests/view_participant.php
@@ -40,27 +40,29 @@ $notes = pg_fetch_assoc($resultNotes);
 
 <div class="d-flex flex-column w-100" style="height: fit-content;">
     <div class="mb-2">
-        <!--<button class="cpca btn" onclick="goBack()"><i class="fa fa-arrow-left"></i> Back</button>-->
+       <button class="cpca btn" onclick="goBack()"><i class="fa fa-arrow-left"></i> Back</button>
     </div>
 	<?php
 function status($timestamp, $activePeriod){
-$statuses = array();
+	$statuses = array();
 	$currentDate =new DateTime("now", new DateTimeZone("America/New_York"));
 	$passedTime =new DateTime($timestamp, new DateTimeZone("America/New_York"));
 	$timeDifference = $passedTime->diff($currentDate); 
 	$timePassed=(int) $timeDifference->format('%a');
 	if($timePassed < $activePeriod){
-		echo "not over $activePeriod days <br>";
-		$statuses["status"] = "active";
+		//echo "not over $activePeriod days <br>";
+		$readableDate= $passedTime->format(' l jS F Y \a\t g:ia ');
+		//print_r($readableDate);
+		$statuses["status"] = "active </span> <i>last seen  : ".toString($readableDate)[0]."</i>";
 		$statuses["class"] = "badge badge-success";
 	}else{
-		echo "over $activePeriod days";
+		//echo "over $activePeriod days";
 		$statuses["status"] = "inactive";
 		$statuses["class"] = "badge badge-secondary";
 	}
 	return $statuses;
 }
-$satuses = status($notes['date'],40);
+$satuses = status($notes['date'],50);
 ?>
     <div class="card" style="max-width: 700px; width: 100%; margin: 0 auto;">
         <div class="card-header">
@@ -74,7 +76,7 @@ $satuses = status($notes['date'],40);
             <hr>
             <div class="pl-3">
                 <p class="participant_name"><b>Name: </b> <?= $participant['firstname']." ".$participant['middleinit']." ".$participant['lastname'] ?></p>
-                <p class="participant_status"><b>Status: </b> <span class="<?= $satuses['class']?>"><?= $satuses['status']?></span> </p>
+                <p class="participant_status"><b>Status: </b> <span class="<?= $satuses['class']?>"><?= $satuses['status']?></p>
                 <p class="participant_notes"><b>Notes: </b> <p class="pl-3">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p></p>
                 <p class="participant_other"><b>Other: </b> Other items to note</p>
                 <p class="participant_contact"><b>Contact: </b></p>
